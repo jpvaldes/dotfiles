ea1a5c1f82524d3b66d86426d8c6f2f6b2c0cdba
Jose P Valdes <sevmpe@gmail.com>
2014-10-09 14:11:03 +0200

    Spelling setup fixes. Flyspell still really slow

--- emacs.d/config/init-flyspell.el
+++ emacs.d/config/init-flyspell.el
@@ -6,4 +6,12 @@
 (dolist (hooks '(emacs-lisp-mode-hook inferior-lisp-mode-hook))
   (add-hook hooks (lambda () (flyspell-prog-mode))))
 
+; Found a possible workaround for flyspell slowness
+(setq-default flyspell-issue-welcome-flag nil)
+; http://www.brool.com/index.php/speeding-up-flyspell-region
+(defadvice flyspell-region (around fast-flyspell-region)
+  (flet ( (sit-for (x) t) )
+    ad-do-it))
+(ad-activate 'flyspell-region)
+
 (provide 'init-flyspell)
--- emacs.d/config/init-ispell.el
+++ emacs.d/config/init-ispell.el
@@ -5,9 +5,72 @@
 (when (eq system-type 'darwin)
   (add-to-list 'exec-path "/usr/local/bin"))
 ; find aspell and hunspell automatically
+; lets do some spellying mistages 
 (cond
+ ((executable-find "hunspell")
+  (setq ispell-program-name "hunspell")
+  (setq ispell-really-hunspell t)
+  (setq ispell-local-dictionary-alist
+        '(
+          (nil
+           "[[:alpha:]]"
+           "[^[:alpha:]]"
+           "[']"
+           t
+           ("-d" "en_US")
+           nil
+           utf-8)
+          ("american"
+           "[[:alpha:]]"
+           "[^[:alpha:]]"
+           "[']"
+           t
+           ("-d" "en_US")
+           nil
+           utf-8)
+          ("english"
+           "[[:alpha:]]"
+           "[^[:alpha:]]"
+           "[']"
+           t
+           ("-d" "en_GB")
+           nil
+           utf-8)
+          ("deutsch"
+           "[[:alpha:]ÄÖÜéäöüß]"
+           "[^[:alpha:]ÄÖÜéäöüß]"
+           "[']"
+           t
+           ("-d" "de_DE")
+           nil
+           iso-8859-1)
+          ("spanish"
+           "[[:alpha:]üÜáéíóúÁÉÍÓÚñÑ]"
+           "[^[:alpha:]üÜáéíóúÁÉÍÓÚñÑ]"
+           "[']"
+           t
+           ("-d" "es_ES")
+           nil
+           iso-8859-1)
+          ))
+  (require-package 'rw-language-and-country-codes)
+  (require-package 'rw-ispell)
+  (require-package 'rw-hunspell)
+  (setq ispell-dictionary "american")
+  (setq rw-hunspell-default-dictionary ispell-dictionary)
+  (setq rw-hunspell-dicpath-list (quote ("~/Library/Spelling")))
+  (setq rw-hunspell-make-dictionary-menu t)
+  (setq rw-hunspell-use-rw-ispell t)
+  (eval-after-load "ispell"
+    '(progn
+       (require 'rw-language-and-country-codes)
+       (require 'rw-ispell)
+       (require 'rw-hunspell)
+       ))
+  (message "Hunspell has been found: using it."))
  ((executable-find "ispell")
   (setq ispell-program-name "ispell")
+  (setq ispell-dictionary "english")
   (message "Using Ispell"))
  ((executable-find "aspell")
   (setq ispell-program-name "aspell")
@@ -15,11 +78,8 @@
   (setq ispell-really-aspell t)
   (setq ispell-list-command "--list"
         ispell-personal-dictionary "~/.emacs.d/personal-dict")
+  (setq ispell-dictionary "english")
   (message "Aspell has been found." ))
- ((executable-find "hunspell") (setq ispell-program-name "hunspell")
-  (setq ispell-really-hunspell t)
-  (message "Hunspell has been found."))
 )
-(setq ispell-dictionary "english")
  
 (provide 'init-ispell)
