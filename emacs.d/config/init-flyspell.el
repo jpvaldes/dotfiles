5545a2f9af56bdd9c515beff03f70a553858c4e1
Jose P Valdes <sevmpe@gmail.com>
2014-10-09 11:24:32 +0200

    No flyspell in python

--- emacs.d/config/init-flyspell.el
+++ emacs.d/config/init-flyspell.el
@@ -1,9 +1,9 @@
 ;; Enable flyspell for text and other files
 (dolist (hooks '(text-mode-hook LaTeX-mode-hook))
   (add-hook hooks (lambda () (flyspell-mode 1))))
-(dolist (hooks '(change-log-mode-hook log-edit-mode-hook))
+(dolist (hooks '(change-log-mode-hook log-edit-mode-hook python-mode-hook))
   (add-hook hooks (lambda () (flyspell-mode -1))))
-(dolist (hooks '(emacs-lisp-mode-hook inferior-lisp-mode-hook python-mode-hook))
+(dolist (hooks '(emacs-lisp-mode-hook inferior-lisp-mode-hook))
   (add-hook hooks (lambda () (flyspell-prog-mode))))
 
 (provide 'init-flyspell)
