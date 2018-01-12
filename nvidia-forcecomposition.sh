879c7f0d8355e50ebb49ba6365925466978c673e
Jose P Valdes <jose.valdes@dzne.de>
2018-01-12 16:01:10 +0100

    Use hostname instead of HOST

--- nvidia-forcecomposition.sh
+++ nvidia-forcecomposition.sh
@@ -3,7 +3,7 @@
 # A little script to run nvidia-settings to force composition and avoid screen
 # tearing. It checks if nvidia-settings exists and it is run in the right
 # computer.
-if [[ -f $(which nvidia-settings) && $HOST = "DZNEL-82393" ]]; then
+if [[ -f $(which nvidia-settings) && $( hostname ) = "DZNEL-82393" ]]; then
     echo +++ nvidia-settings and host found
     echo +++ Forcing composition on DFP-0 and DFP-2
     nvidia-settings --assign CurrentMetaMode="DFP-0: nvidia-auto-select +0+0 {ForceCompositionPipeline=On}, DFP-2: nvidia-auto-select +1920+0 {ForceCompositionPipeline=On}"
