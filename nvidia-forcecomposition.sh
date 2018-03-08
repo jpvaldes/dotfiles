8894a5c0e267ca0fae113bec5dbe56b595f9125d
Jose P Valdes <jose.valdes@dzne.de>
2018-03-08 08:54:44 +0100

    Better computer id check

--- nvidia-forcecomposition.sh
+++ nvidia-forcecomposition.sh
@@ -3,7 +3,8 @@
 # A little script to run nvidia-settings to force composition and avoid screen
 # tearing. It checks if nvidia-settings exists and it is run in the right
 # computer.
-if [[ -f $(which nvidia-settings) && $( hostname ) = "DZNEL-82393" ]]; then
+THIS_COMPUTER=$(hostname)
+if [[ -f $(which nvidia-settings) && ${THIS_COMPUTER#-*} = "82393" ]]; then
     echo +++ nvidia-settings and host found
     echo +++ Forcing composition on DFP-0 and DFP-2
     nvidia-settings --assign CurrentMetaMode="DFP-0: nvidia-auto-select +0+0 {ForceCompositionPipeline=On}, DFP-2: nvidia-auto-select +1920+0 {ForceCompositionPipeline=On}"
@@ -11,7 +12,7 @@ if [[ -f $(which nvidia-settings) && $( hostname ) = "DZNEL-82393" ]]; then
     exit 0
 else
     echo +++ Either nvidia-settings was not found or
-    echo +++ this computer is not DZNEL-82393
+    echo +++ this computer is not 82393
     echo +++ Doing nothing
     exit 1
 fi
